40f6e4faec14dc938ff1d517cd34e218357c87d3
Jace <sockgnome@att.net>
2024-06-25 14:07:47 -0500

    Added .env use and mongodb to the project.

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
-const port = 3000
+const cors = require('cors');
+const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello Jace, You Are My World!')
-})
+app.use(cors());
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Server listening on port: ${port}`)
-})
+// Default Route
+app.get('*', (req, res) => {
+  res.status(404).send('These are not the droids you are looking for.');
+});
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
