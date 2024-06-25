7aa00efe063a42e8d079e0d015c57e92456cf2b1
Jace <sockgnome@att.net>
2024-06-25 17:18:38 -0500

    Added CORS to express

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const protectedRoutes = require('./routes/protectedRoutes');
 
 dotenv.config();
 
 const app = express();
 
+const corsOptions = {
+  origin: 'https://medtrk.netlify.app',
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use('/api', protectedRoutes);
 
 const port = process.env.PORT || 3001;
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
