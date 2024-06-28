4e2fee4027c592ddd4daeb5a2c4ac37962341e99
Jace <sockgnome@att.net>
2024-06-27 21:41:32 -0500

    Update CORS configuration for server.js to allow requests from specific origin

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ app.use(cors({
 }));
 
 // Having issues with CORS, debug stuff...
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://medtrk.netlify.app');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  console.log(`Request received: ${req.method} ${req.url}`);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://medtrk.netlify.app');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   console.log(`Request received: ${req.method} ${req.url}`);
+//   next();
+// });
 
 // Middleware to parse JSON
 app.use(express.json({ extended: false }));
